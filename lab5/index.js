a47f6bb20c1b74b656e5ae82288bae8263966a15
whoiscan <jamhurj@gmail.com>
2022-06-24 23:26:06 -0500

    lab 5 done

--- lab5/index.js
+++ lab5/index.js
@@ -1,7 +1,18 @@
-let arr = [10, 20, 30, 40, 50];
+//Problem1
+const arr = [10, 20, 30, 40, 50];
 
 function sum(array) {
     return array.filter(a => a > 20)
         .reduce((x, y) => x + y);
 }
+
 console.log(sum(arr));
+
+//Problem 2
+const arr1 = ['abcde', 'vwxyz', 'lmn', 'aqwerty'];
+
+function getNewArray(array) {
+    return array.filter(a => a.length >= 5 && a.includes('a'));
+}
+
+console.log(getNewArray(arr1));
