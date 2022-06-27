e8222b0b95920dba22a37440d3ef8925fa3f3857
whoiscan <jamhurj@gmail.com>
2022-06-26 21:54:57 -0500

    lab 5 done

--- lab5/index.js
+++ lab5/index.js
@@ -4,7 +4,7 @@ const arr = [10, 20, 30, 40, 50];
 function sum(array) {
     return array.filter(a => a > 20)
         .reduce((x, y) => x + y);
-}
+};
 
 console.log(sum(arr));
 
@@ -13,6 +13,6 @@ const arr1 = ['abcde', 'vwxyz', 'lmn', 'aqwerty'];
 
 function getNewArray(array) {
     return array.filter(a => a.length >= 5 && a.includes('a'));
-}
+};
 
 console.log(getNewArray(arr1));
